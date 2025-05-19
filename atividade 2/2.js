c3f9650f750fc2ef47a5e5f7fee7bc2feec0dbf5
ArthurLorenzo230 <arthurlorenzo@colegiopaulodetarso.com.br>
2025-05-19 16:18:02 +0000

    retghj

---
+++ atividade 2/2.js
@@ -0,0 +1,9 @@
+// Função que compara dois números e retorna true se o primeiro for maior
+function ehMaior(a, b) {
+    return a > b;
+}
+
+//teste
+console.log(ehMaior(10, 5));   // true
+console.log(ehMaior(2, 8));    // false
+console.log(ehMaior(7, 7));    // false
---
+++ atividade 2/4.js
@@ -0,0 +1,19 @@
+<!-- btn-alert.html -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Botão de Alerta</title>
+</head>
+<body>
+
+  <button onclick="mostrarMensagem()">Clique aqui</button>
+
+  <script>
+    function mostrarMensagem() {
+      alert("Você clicou no botão!");
+    }
+  </script>
+
+</body>
+</html>
